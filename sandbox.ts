10a9a309fbf000fe0bb4edd8dab233be0332b963
Ömer Mercan <70708913+O-Mercan@users.noreply.github.com>
2025-01-17 11:05:45 +0300

    lecture 4: objects and arrays

--- sandbox.ts
+++ sandbox.ts
@@ -1,18 +1,38 @@
-let character = 'mario';
-let age = 30;
-let isBlackBelt= false;
+// arrays
+let names = ['luigi', 'mario', 'yoshi'];
 
-//character = 20;
-character = 'luigi';
+names.push('toad');
+//names.push(3);
+//names[0] = 3;
 
-//age = 'yoshi';
-age = 40;
+let numbers = [10, 20, 30, 40];
 
-//isBlackBelt = 'yes';
-isBlackBelt = true;
+numbers.push(25);
+// numbers.push('shaun');
+// numbers[1] = 'shaun';
 
-const circle = (diameter: number) => {
-    return diameter * Math.PI;
+let mixed = ['ken', 4 , 'chun-li', 8, 9, true];
+
+mixed.push('ryu');
+mixed.push(10);
+mixed.push(false);
+mixed[0] = 3;
+
+// object
+let ninja = {
+    name: 'mario',
+    belt: 'black',
+    age: 25,
+};
+
+ninja.age = 40;
+ninja.name = 'ryu';
+//ninja.age = '30';
+// ninja.skills = ['fighting', 'sneaking'];
+
+ninja = {
+    name: 'yoshi',
+    belt: 'orange',
+    age: 50,
+    //skills: ['fihting']
 }
-
-console.log(circle(2.2));
