0ca0412672602c8cd8ae2347c54390c2470b6122
Ömer Mercan <70708913+O-Mercan@users.noreply.github.com>
2025-01-19 15:22:26 +0300

    added lecture 15: Interfaces

--- public/app.js
+++ public/app.js
@@ -1,3 +1,19 @@
+const me = {
+    name: "shaun",
+    age: 25,
+    speak(text) {
+        console.log(text);
+    },
+    spend(amount) {
+        console.log('I spent', amount);
+        return amount;
+    },
+};
+const greetPerson = (person) => {
+    console.log('hello', person.name);
+};
+greetPerson(me);
+console.log(me);
 import { Invoice } from './classes/Invoice.js';
 const invOne = new Invoice("mario", "work on the mario website", 200);
 const invTwo = new Invoice("luigi", "work on the luigi website", 300);
@@ -7,12 +23,13 @@ invoices.push(invTwo);
 invoices.forEach(inv => {
     console.log(inv.client, inv.amount, inv.format());
 });
+const form = document.querySelector('.new-item-form');
 // INPUTS
 const type = document.querySelector("#type");
 const tofrom = document.querySelector("#tofrom");
 const details = document.querySelector("#details");
 const amount = document.querySelector("#amount");
-tofrom.addEventListener("submit", (e) => {
+form.addEventListener("submit", (e) => {
     e.preventDefault();
     console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
 });
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,31 @@
+// INTERFACES
+interface IsPerson {
+    name: string;
+    age: number;
+    speak(a: string): void;
+    spend(a: number):number;
+}
+
+const me: IsPerson = {
+    name: "shaun",
+    age: 25,
+    speak(text: string): void{
+        console.log(text);
+    },
+    spend(amount: number): number {
+        console.log('I spent', amount);
+        return amount;
+    },
+};
+
+const greetPerson = (person: IsPerson) => {
+    console.log('hello', person.name)
+};
+
+greetPerson(me);
+
+console.log(me);
+
 import {Invoice} from './classes/Invoice.js';
 
 const invOne = new Invoice("mario", "work on the mario website", 200);
@@ -12,13 +40,15 @@ invoices.forEach( inv => {
 });
 
 
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+
 // INPUTS
 const type = document.querySelector("#type")! as HTMLSelectElement;
 const tofrom = document.querySelector("#tofrom")! as HTMLInputElement;
 const details = document.querySelector("#details")! as HTMLInputElement;
 const amount = document.querySelector("#amount")! as HTMLInputElement;
 
-tofrom.addEventListener("submit", (e: Event) => {
+form.addEventListener("submit", (e: Event) => {
     e.preventDefault();
 
     console.log(
